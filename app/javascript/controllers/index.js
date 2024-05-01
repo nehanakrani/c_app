0882139c486e19eb19051fd4df2266257ac195e9
nehanakrani <nehanakrani04@gmail.com>
2024-05-01 17:48:11 +0100

    Addded stimulus

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,8 +4,10 @@ import { application } from "controllers/application"
 
 // Eager load all controllers defined in the import map under controllers/**/*_controller
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
+import StimulusReflex from "stimulus_reflex"; // <-- add this
+
 eagerLoadControllersFrom("controllers", application)
 
-// Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
-// import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
-// lazyLoadControllersFrom("controllers", application)
+// initialize StimulusReflex w/top-level await
+const consumer = await cable.getConsumer()
+StimulusReflex.initialize(application, { consumer, debug: true });
--- config/importmap.rb
+++ config/importmap.rb
@@ -10,3 +10,8 @@ pin 'bootstrap', to: 'bootstrap.min.js', preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "stimulus_reflex" # @3.5.0
+pin "@rails/actioncable", to: "@rails--actioncable.js" # @7.1.3
+pin "cable_ready" # @5.0.0
+pin "morphdom" # @2.7.2
+pin "stimulus" # @3.2.2
